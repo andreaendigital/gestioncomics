89c7af3a7aba436e0aaed808e55af7e5a421f574
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-06-30 12:30:57 -0400

    creación de archivos, paquetes, inicialización del proyecto

---
+++ src/main/java/com/comicstore/comiccollectorsystem/ComicCollectorSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.comicstore.comiccollectorsystem;
+
+/**
+ *
+ * @author Andrea
+ */
+public class ComicCollectorSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
